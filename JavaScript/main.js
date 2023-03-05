02bb2f667f18e0ee16e553371f51273a7228138c
christian <agosti400@gmail.com>
2023-03-05 20:59:32 -0300

    envio de ejercicios en una website sencilla

---
+++ JavaScript/main.js
@@ -0,0 +1,21 @@
+
+
+
+let intro = prompt('introduce tu nombre')
+
+
+
+alert('Bienvenido a mi humilde e incompleta website' + intro)
+
+
+
+
+
+
+
+/*
+Construir un proyecto base como el de la clase.
+Crear sus repositorios y subirlos (siguiendo los pasos que se hicieron en la clase de hoy.)
+Crear una pagina que se presenten ustedes.
+Creen una app.js que utilice alert y prompt para que finalmente mediante la alerta salude.
+*/
